2154ea1cb96e94e0cbbe9893ecb0848d22d93437
Title <Title@TITLE-PC>
2022-09-07 17:12:38 +0700

    edit 2022-09-07 17.12

---
+++ testcsharp/testcsharp_01/CodeLearning_001/frmLn001.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace testcsharp_01.CodeLearning_001
+{
+    public partial class frmLn001 : Form
+    {
+        public frmLn001()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- testcsharp/testcsharp_01/Program.cs
+++ testcsharp/testcsharp_01/Program.cs
@@ -16,7 +16,7 @@ namespace testcsharp_01
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new CodeLearning_001.frmLn001());
         }
     }
 }
